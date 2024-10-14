5b08d2a1b664a8fbf4f1b352eb37354dbc550157
Luis Castillo <luigercm66@gmail.com>
2024-10-14 20:45:06 +0200

    commit

--- alpha_app_modular.py
+++ alpha_app_modular.py
@@ -62,7 +62,7 @@ if 'df_clean_data' not in st.session_state:
 
 # cover image
 enlace_img="https://raw.githubusercontent.com/Uplyuz/PolarWeb/af24864c44ed44e486a19ce6c01b7cf1078899bd/.streamlit/images/portrait.PNG"
-st.image(enlace_img) 
+st.image(enlace_img, use_column_width=True) 
 # header
 st.header("Your Personalized X-Sentiment Analysis")
 
