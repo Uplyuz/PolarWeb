7841010b17fc02442e08248c55e95299ebad3b70
Luis Castillo <luigercm66@gmail.com>
2024-10-14 20:28:36 +0200

    imagen

--- alpha_app_modular.py
+++ alpha_app_modular.py
@@ -61,7 +61,7 @@ if 'df_clean_data' not in st.session_state:
     st.session_state.df_clean_data = None
 
 # cover image
-# st.image(r"../.streamlit/images/portrait.PNG", use_column_width=True) 
+st.image(r"https://github.com/Uplyuz/PolarWeb/blob/af24864c44ed44e486a19ce6c01b7cf1078899bd/.streamlit/images/portrait.PNG", use_column_width=True) 
 # header
 st.header("Your Personalized X-Sentiment Analysis")
 
