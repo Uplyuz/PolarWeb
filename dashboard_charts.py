d66812430b5b7e5a7f4194b4326fb0d749127ce2
Agus <lujan.agustin@gmail.com>
2024-10-15 22:53:43 +0200

    dif_layout

--- dashboard_charts.py
+++ dashboard_charts.py
@@ -13,13 +13,13 @@ def plot_wordcloud(df):
     positive_words = " ".join(df['Tweet'][df['Sentiment'] == 'Positive'])
     negative_words = " ".join(df['Tweet'][df['Sentiment'] == 'Negative'])
 
-    wordcloud = WordCloud(width=875, height=900, background_color="black", max_words=50, min_font_size=20, random_state=42)\
+    wordcloud = WordCloud(width=875, height=900, background_color="lightgrey", max_words=50, min_font_size=20, random_state=42)\
         .generate(positive_words)
     
     wordcloud2 = WordCloud(width=875, height=900, background_color="black", max_words=50, min_font_size=20, random_state=42)\
         .generate(negative_words)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 9), facecolor=None)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(18, 9), facecolor=None)
     ax1.imshow(wordcloud, interpolation='bilinear')
     ax2.imshow(wordcloud2, interpolation='bilinear')
     ax1.set_title('Positive Tweets', fontsize=20)
