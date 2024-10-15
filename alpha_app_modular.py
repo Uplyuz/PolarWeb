3c63d6ed81f24937e1cee9cb71158c820ba822fa
Agus <lujan.agustin@gmail.com>
2024-10-16 00:12:57 +0200

    changes in dashboard

--- alpha_app_modular.py
+++ alpha_app_modular.py
@@ -197,14 +197,15 @@ with tab3:
         st.write("Sentiment Analysis Results:")
         
         from dashboard_charts import plot_wordcloud, sentiment_dist, format_data_model_output, obtain_summary
-        st.dataframe(format_data_model_output(df_clean_data)) #chequear
+        aux_01 = format_data_model_output(df_clean_data) #chequear
+        st.write(aux_01) #chequear
         sentiment_dist(df_clean_data)
         total_tweets = len(df_clean_data)
         total_likes = df_clean_data['Tweet_Likes'].sum()
         st.write(f"Total Tweets Analyzed: {total_tweets}")
         st.write(f"Total Likes on Tweets: {total_likes}")
         plot_wordcloud(df_clean_data)
-        st.dataframe(obtain_summary(df_clean_data)) #chequear
+        st.write(obtain_summary(df_clean_data)) #chequear
             
     else:
         st.warning('Perform a search in tab "Set-up your Search" to get a personalized data analysis.')
