7432c36c2186f9a206de53299d1f914c24f46d66
Agus <lujan.agustin@gmail.com>
2024-10-16 17:37:47 +0200

    dashb 1601

--- alpha_app_modular.py
+++ alpha_app_modular.py
@@ -207,7 +207,7 @@ with tab3:
         st.write(f"Total Tweets Analyzed: {total_tweets}")
         st.write(f"Total Likes on Tweets: {total_likes}")
         plot_wordcloud(df_clean_data)
-        obtain_summary(df_clean_data) #chequear
-            
+        aux_02 = obtain_summary(df_clean_data) #chequear
+        st.writ(aux_02)    
     else:
         st.warning('Perform a search in tab "Set-up your Search" to get a personalized data analysis.')
